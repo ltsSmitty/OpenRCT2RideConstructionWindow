9dece8499268ca3b14fe688575c9d0308783c88b
Smitty Penman <smitty.penman@gmail.com>
2022-12-25 06:08:53 -0500

    tweak config to fit my needs

--- gulp/build/build.ts
+++ gulp/build/build.ts
@@ -3,14 +3,14 @@ import { spawn } from 'child_process';
 import config from '~/config';
 
 export default function build(): ChildProcess {
-  const spawned = spawn(
-    'rollup',
-    ['--config', `rollup.config.${config.getString('NODE_ENV')}.ts`, '--configPlugin', 'typescript'],
-    { shell: true }
-  );
+	const spawned = spawn(
+		'rollup',
+		['--config', `rollup.config.${config.getString('NODE_ENV')}.ts`, '--configPlugin', 'typescript'],
+		{ shell: true }
+	);
 
-  spawned.stderr.pipe(process.stderr);
-  spawned.stdout.pipe(process.stdout);
+	spawned.stderr.pipe(process.stderr);
+	spawned.stdout.pipe(process.stdout);
 
-  return spawned;
+	return spawned;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../lib/openrct2.d.ts" />
+
 import config from './config';
 import main from './main';
 
 registerPlugin({
-  name: config.getString('MOD_NAME'),
-  version: '1.0',
-  authors: [config.getString('MOD_AUTHOR')],
-  type: 'local',
-  licence: 'MIT',
-  main,
+    name: config.getString('MOD_NAME'),
+    version: '1.0',
+    authors: [config.getString('MOD_AUTHOR')],
+    type: 'local',
+    licence: 'MIT',
+    main,
 });
